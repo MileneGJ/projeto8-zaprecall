1fe7ae39cdfe35fee247b712011b127b3e473bf0
milenegj <milene.jannetti@gmail.com>
2022-05-11 19:12:23 -0500

    Welcome screen OK

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,10 +1,49 @@
 export default function Questions () {
+    const deck = [{
+        question:"O que é JSX?",
+        answer:"Uma extensão de linguagem do JavaScript"
+    },{
+        question:"O React é __",
+        answer:"Uma biblioteca JavaScript para construção de interfaces"
+    },{
+        question:"Componentes devem iniciar com __",
+        answer:"Letra maiúscula"
+    },{
+        question:"Podemos colocar __ dentro do JSX",
+        answer:"Expressões"
+    },{
+        question:"O ReactDOM nos ajuda __",
+        answer:"Interagindo com a DOM para colocar componentes React na mesma"
+    },{
+        question:"Usamos o npm para __",
+        answer:"Gerenciar os pacotes necessários e suas dependências"
+    },{
+        question:"Usamos props para __",
+        answer:"Passar diferentes informações para componentes"
+    },{
+        question:"Usamos estado (state) para __",
+        answer:"Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"
+    }]
+
+    function Question ({index,question,answer}){
+        return(
+        <li>
+                <h2>Pergunta {index+1}</h2>
+                <ion-icon name="play-outline"></ion-icon>
+                <p style={{display:"none"}}>{question}</p>
+                <p style={{display:"none"}}>{answer}</p>
+            </li>
+        )
+    }
     return (
         <ul className="questions">
-            <li>
-                <h2>Pergunta 1</h2>
-                <ion-icon name="play-outline"></ion-icon>
-            </li>
+            {deck.map((QetA,index)=>
+            <Question 
+            key={index}
+            index={index}
+            question={QetA.question}
+            answer={QetA.answer}
+             />)}
         </ul>
     )
 }
